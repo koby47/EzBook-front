87cbf4dd97b5775e11fcf8710c9bcde5e4ce4efc
koby47 <samuelmensahquaye@gmail.com>
2025-05-27 00:24:21 +0000

    error

--- services/firebase.js
+++ services/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-// Replace with your actual Firebase config
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -11,13 +10,7 @@ const firebaseConfig = {
   messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 
-  // apiKey: "AIzaSyDePBa2SPq5qcbhVQFTycfp1ObT8PD6y2k",
-  // authDomain: "ezbook-e8018.firebaseapp.com",
-  // projectId: "ezbook-e8018",
-  // storageBucket: "ezbook-e8018.firebasestorage.app",
-  // messagingSenderId: "839002024332",
-  // appId: "1:839002024332:web:3a1d0806e2fc4c5f944ea9",
- 
+  
 
 };
 
